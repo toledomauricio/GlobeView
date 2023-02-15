222e4aab3a5b1a6cb79acd726b57d8c15ba4237e
glip <nork1337@gmail.com>
2023-02-15 02:51:56 -0300

    Add WebView call

--- app/src/main/java/com/globeview/MainActivity.kt
+++ app/src/main/java/com/globeview/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.globeview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    @SuppressLint("SetJavaScriptEnabled")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.loadUrl("https://example.com/")
     }
 }
